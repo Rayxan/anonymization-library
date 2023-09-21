61e1fcf7bbd92fd1fefdc21c90dad25766f4973b
Raylan <raylanwork@gmail.com>
2023-09-21 09:34:39 -0300

    Realizando substituição de vários CPFs e tradução de comentários

--- alteraArquivo.py
+++ alteraArquivo.py
@@ -1,63 +1,40 @@
 import aspose.pdf as pdf
 import re
+from pdfminer.high_level import extract_text
 
-from pdfminer.high_level import extract_pages, extract_text
-
+# Usando a biblioteca pdfminer para ler e extrair CPFs do PDF
 text = extract_text("example2.pdf")
-# print(text)
 
+# Regex referente ao formato do CPF
 pattern = re.compile(r'\d{3}\.\d{3}\.\d{3}-\d{2}')
 matches = pattern.findall(text)
 
-print(matches)
-
-###########################################################
-
+# Usando a biblioteca aspose.pdf para ler o PDF e fazer a substituição
+first_loop = True
 for match in matches:
-    # Open the PDF document
-    inputPDFFile = pdf.Document("example2.pdf")
+    # Documento PDF é aberto
+    # Condição criada para atualizar o documento a cada loop
+    if(first_loop):
+        inputPDFFile = pdf.Document("example2.pdf")
+        first_loop = False
+    else:
+        inputPDFFile = pdf.Document("output.pdf") 
 
-    # Instantiate a TextFragmentAbsorber object
-    # matches[0] has the first CPF
+    # Objeto TextFragmentAbsorber é instanciado
     txtAbsorber = pdf.text.TextFragmentAbsorber(match)
 
-    # Search text
+    # Procura texto no documento PDF (não sei que comentário utilizar aqui)
     inputPDFFile.pages.accept(txtAbsorber)
 
-    # Get reference to the found list
+    # Obtém uma referência à lista de fragmentos de texto encontrados
     textFragmentCollection = txtAbsorber.text_fragments
 
-    # Parse all the searched text fragments
+    # Analisa todos os fragmentos de texto encontrados
     for txtFragment in textFragmentCollection:
-        txtFragment.text = "*"
+        # Substitui o texto encontrado
+        txtFragment.text = "***.***.***-**"
 
-    # Save the output PDF
+    # Salva em um novo pdf
     inputPDFFile.save("output.pdf")
 
-    print("Text found and replaced successfully")
-
-##########################################################################
-# # VERSÃO SEM O FOREACH 
-# # (DESCOMENTAR ESSE E COMENTAR A DE CIMA A PARTIR DO FOREACH (LINHAS 16 A 37))
-
-# # Open the PDF document
-# inputPDFFile = pdf.Document("example2.pdf")
-
-# # Instantiate a TextFragmentAbsorber object
-# # matches[0] has the first CPF
-# txtAbsorber = pdf.text.TextFragmentAbsorber(matches[0])
-
-# # Search text
-# inputPDFFile.pages.accept(txtAbsorber)
-
-# # Get reference to the found list
-# textFragmentCollection = txtAbsorber.text_fragments
-
-# # Parse all the searched text fragments
-# for txtFragment in textFragmentCollection:
-#     txtFragment.text = "*"
-
-# # Save the output PDF
-# inputPDFFile.save("output.pdf")
-
-# print("Text found and replaced successfully")
+    print("Texto encontrado e substituído com sucesso")

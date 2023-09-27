6cd97667b9da9a82a44dace814c6902c65a2d758
KawaiiStheno <stefanoluppi@hotmail.com>
2023-09-27 18:17:51 -0300

    Regex para anonimização de Telefone, Data, CEP e Email adicionados

--- regexDadosPessoais.py
+++ regexDadosPessoais.py
@@ -2,3 +2,11 @@ import re
 
 class RegexDadosPessoais:
     regexCPF = re.compile(r'\d{3}\.\d{3}\.\d{3}-\d{2}')
+
+    regexTelef = re.compile(r'[(]\d{2}[)][ ]*\d{4}|\d{5}-\d{4}')
+
+    regexData = re.compile(r'\d{2}[/]\d{2}[/]\d{2}|\d{4}')
+
+    regexCEP = re.compile(r'\d{5}[-]\d{3}')
+
+    regexEmail = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
